4e3d9fe87182e043715526bfdaf097f0b9f56adb
EdgarDev2 <edgar.poot.ku22@gmail.com>
2024-11-12 15:19:14 -0600

    Se agrega la página de inicia y página acerca de

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,51 +2,30 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+use yii\helpers\Html;
+
+$this->title = 'Inicio';
+$this->registerCssFile('@web/css/custom_styles.css', ['depends' => [\yii\web\YiiAsset::class]]);
 ?>
+
 <div class="site-index">
-    <div class="p-5 mb-4 bg-transparent rounded-3">
-        <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-        </div>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+    <div class="body-content d-flex flex-column justify-content-center" style="min-height: 80vh;">
+        <section class="row">
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-center">
+                <div class="text text-center text-md-start">
+                    <h1 class="display-4 text-dark">MONITOREO WEB EN TIEMPO REAL DE HORTALIZAS</h1>
+                    <p class="lead text-dark">En el Instituto Tecnológico Superior de Valladolid se elaboró este proyecto con el fin de monitorear
+                        las variables ambientales cruciales...</p>
+                    <div class="buttons mt-4">
+                        <?= Html::a('SEGUIR LEYENDO', ['/site/about'], ['class' => 'btn btn-outline-success btn-lg', 'title' => 'Más información sobre el proyecto']) ?>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <div class="col-12 col-md-6 d-flex justify-content-center align-items-center mt-4 mt-md-0">
+                <div class="image position-relative">
+                    <?= Html::img('@web/images/cilantro1.png', ['class' => 'img-fluid rounded', 'alt' => 'Imagen de cilantro, utilizado en el monitoreo de hortalizas.']) ?>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
+        </section>
     </div>
 </div>
